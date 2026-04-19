7e4ea0bdea045f79fb7faf9fd29b9dc3b35c4af8
PabloC0608 <pablojosue0608@gmail.com>
2026-04-19 18:12:32 -0500

    ingresoDAO-completo

--- CooperativaMercado/CooperativaMercado/Repository/Dao/IngresoDAO.cs
+++ CooperativaMercado/CooperativaMercado/Repository/Dao/IngresoDAO.cs
@@ -1,6 +1,35 @@
-﻿namespace CooperativaMercado.Repository.Dao
+﻿using CooperativaMercado.Model;
+using CooperativaMercado.Repository.Interfaces;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CooperativaMercado.Repository.Dao
 {
-    public class IngresoDAO
+    public class IngresoDAO : IIngreso
     {
+        private readonly string connectionString;
+
+        public IngresoDAO()
+        {
+            connectionString = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build()
+                .GetConnectionString("database");
+        }
+
+        public int RegistrarIngreso(IngresoDiario ingresoDiario)
+        {
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("sp_RegistrarIngreso", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IdPuesto", ingresoDiario.IdPuesto);
+                cmd.Parameters.AddWithValue("@Fecha", ingresoDiario.Fecha);
+                cmd.Parameters.AddWithValue("@Monto", ingresoDiario.Monto);
+                cmd.Parameters.AddWithValue("@IdUsuario", ingresoDiario.IdUsuario);
+                return cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
--- CooperativaMercado/CooperativaMercado/Repository/Interfaces/IIngreso.cs
+++ CooperativaMercado/CooperativaMercado/Repository/Interfaces/IIngreso.cs
@@ -1,6 +1,9 @@
-﻿namespace CooperativaMercado.Repository.Interfaces
+﻿using CooperativaMercado.Model;
+
+namespace CooperativaMercado.Repository.Interfaces
 {
     public interface IIngreso
     {
+        int RegistrarIngreso(IngresoDiario ingresoDiario);
     }
 }
