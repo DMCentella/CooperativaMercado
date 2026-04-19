0ed92022a719319386c9d36b4f56285c3ad37db5
PabloC0608 <pablojosue0608@gmail.com>
2026-04-18 21:52:09 -0500

    DeudaDAO-Completo

--- CooperativaMercado/CooperativaMercado/Repository/Dao/DeudaDAO.cs
+++ CooperativaMercado/CooperativaMercado/Repository/Dao/DeudaDAO.cs
@@ -1,6 +1,67 @@
-﻿namespace CooperativaMercado.Repository.Dao
+﻿using CooperativaMercado.Model;
+using CooperativaMercado.Repository.Interfaces;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CooperativaMercado.Repository.Dao
 {
-    public class DeudaDAO
+    public class DeudaDAO : IDeuda
     {
+        private readonly string connectionString;
+
+        public DeudaDAO()
+        {
+            connectionString = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build()
+                .GetConnectionString("database");
+        }
+
+        public int Actualizar(int idDeuda, Deuda deuda)
+        {
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("sp_ActualizarDeuda", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IdDeuda", idDeuda);
+                cmd.Parameters.AddWithValue("@Descripcion", (object)deuda.Descripcion ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@Monto", deuda.Monto);
+                cmd.Parameters.AddWithValue("@FechaVencimiento", (object)deuda.FechaVencimiento ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@Estado", deuda.Estado);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int Crear(Deuda deuda)
+        {
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("sp_CrearDeuda", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IdPuesto", deuda.IdPuesto);
+                cmd.Parameters.AddWithValue("@IdTipoDeuda", deuda.IdTipoDeuda);
+                cmd.Parameters.AddWithValue("@Descripcion", (object)deuda.Descripcion ?? DBNull.Value);
+                cmd.Parameters.AddWithValue("@Monto", deuda.Monto);
+                cmd.Parameters.AddWithValue("@Mes", deuda.Mes);
+                cmd.Parameters.AddWithValue("@Anio", deuda.Anio);
+                cmd.Parameters.AddWithValue("@FechaVencimiento", (object)deuda.FechaVencimiento ?? DBNull.Value);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int GenerarAlquilerMensual(int mes, int anio)
+        {
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("sp_GenerarAlquilerMensual", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Mes", mes);
+                cmd.Parameters.AddWithValue("@Anio", anio);
+                return cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
--- CooperativaMercado/CooperativaMercado/Repository/Interfaces/IDeuda.cs
+++ CooperativaMercado/CooperativaMercado/Repository/Interfaces/IDeuda.cs
@@ -4,6 +4,8 @@ namespace CooperativaMercado.Repository.Interfaces
 {
     public interface IDeuda
     {
- 
+        int Crear(Deuda deuda);
+        int Actualizar(int idDeuda, Deuda deuda);
+        int GenerarAlquilerMensual(int mes, int anio);
     }
 }
